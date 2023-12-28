61fb00f9178082e6d9fd31a3dcedc8327f63b2cd
c0lin0Brien <colinpobrien04@gmail.com>
2023-12-27 17:24:51 -0800

    Implemented drawing functionality

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-let sps = 16;
 const grid = document.querySelector(".grid");
-for (let i = 0; i < sps; i++) {
-    let lat = document.createElement("div");
-    lat.classList.add("lat");
-    // lat.textContent = "This is a lateral row!"
-    for (let j = 0; j < sps; j++) {
-        let lon = document.createElement("div");
-        lon.classList.add("lon");
-        // lon.textContent = "box"
-        lat.appendChild(lon);
+// Creates grid (default size is 16x16)
+function makeGrid(sps) {
+    for (let i = 0; i < sps; i++) {
+        let lat = document.createElement("div");
+        lat.classList.add("lat");
+        for (let j = 0; j < sps; j++) {
+            let lon = document.createElement("div");
+            lon.classList.add("lon");
+            // Listener for coloring boxes
+            lon.addEventListener("mouseover", function() {
+                lon.style.backgroundColor = "#000000";
+            });
+            lat.appendChild(lon);
+        }
+        grid.appendChild(lat);
     }
-    grid.appendChild(lat);
-}
+}  
+makeGrid(16);
